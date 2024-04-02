5f36768ff562ae793f5b3f987f48987a7d5eb3ce
Gustavo-caetano <gustavocaetno@gmail.com>
2024-04-02 15:07:56 -0300

    fix:Adição de cors

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from pdfApi import PdfApi
 from documentsApi import DocumentsApi
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
