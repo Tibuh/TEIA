413e15b6409e6e1e0ebfe63de71265f8522096b9
Gabriel Oliveira <66384277+G4bzz@users.noreply.github.com>
2024-04-06 22:21:48 -0300

    doc: adiciona comentários no código

--- src/main.py
+++ src/main.py
@@ -3,32 +3,44 @@ from pdfApi import PdfApi
 from documentsApi import DocumentsApi
 from flask_cors import CORS
 
+# Este arquivo main.py é responsável por criar a API da aplicação com o Flask
+
 app = Flask(__name__)
 CORS(app)
 
+#Rota padrão da API
 @app.route("/")
 def hello_world():
-    return "<p>"+"Ta rodando pae"+"</p>"
+	return "<p>"+"API no ar!"+"</p>"
 
+#Rota para requisitar, a partir de uma pergunta, uma resposta baseada no PDF
 @app.post("/question")
 def question_post():
-    data = request.json
+	# Obtém o body da requisição
+	data = request.json
 
-    pathName = data.get('document')    
-    api = PdfApi(pathName)
+	# Instancia a API de QA-RAG
+	api = PdfApi()
 
-    response = api.pdf_response(data.get('question'))
+	# Executa o pipeline de QA-RAG passando a pergunta que veio no body como parâmetro
+	response = api.pdf_response(data.get('question'))
 
-    return jsonify(
-        response = response["result"],
-    )
+	# Retorna a resposta 
+	return jsonify(
+		response = response["result"],
+	)
+
+# Endpoint para listar os PDFs que estão armazenados no backend
+# OBS.: este endpoint foi criado pensando em uma futura melhoria da aplicação
 @app.get("/documents")
 def documents_availables():
-    listDocuments = DocumentsApi.Documents_avaibles()
+	# Lista os documentos presentes no backend
+	listDocuments = DocumentsApi.Documents_avaibles()
 
-    return jsonify(
-        documents = listDocuments
-    )
+	# Retorna os documentos
+	return jsonify(
+			documents = listDocuments
+	)
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000, host='0.0.0.0')
+	app.run(debug=False, port=5000, host='0.0.0.0')
